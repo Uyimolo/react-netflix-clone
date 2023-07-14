0be92e4c613133e1a6ebcfb2448241d0e9a3a3fa
uyimolo <uyijustindavid@gmail.com>
2023-07-14 21:37:30 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return <div className="">
+    <Routes>
+      <Route />
+    </Routes>
+  </div>;
+}
+
+export default App;
